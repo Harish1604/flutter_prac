ee6a26e27da17c2b26296d4c1f915601593564b3
Harish1604 <harish16042005@gmail.com>
2025-08-27 20:23:30 +0530

    practise mad

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -6,7 +6,6 @@ class landingPage extends StatefulWidget {
   @override
   _landingPage createState() => _landingPage();
 }
-
 class _landingPage extends State<landingPage> {
   int selctedIndex = 0;
   @override
